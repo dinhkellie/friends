f04511fb239a754a3e58e9e4a85e7d67cbddc1d0
Michael Yang <themichaelyang@gmail.com>
2018-10-20 02:00:32 -0400

    added basic fb login, added https to dev server

--- src/app.js
+++ src/app.js
@@ -2,7 +2,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import * as secrets from './secrets.js';
 
-ReactDOM.render(<p>
-	This is my boilerplate
-</p>, document.getElementById('app'));
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    // this.checkLoginState = this.checkLoginState.bind(this);
+  }
+
+  componentDidMount() {
+    console.log('app id: ' + secrets.appId);
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId            : secrets.appId,
+        autoLogAppEvents : true,
+        xfbml            : true,
+        version          : 'v3.1'
+      });
+    };
+ 
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }
+
+  test() {
+    FB.login(function(response) {
+    if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+    } else {
+     console.log('User cancelled login or did not fully authorize.');
+    }
+    });
+  }
+  // checkLoginState() {
+  //   console.log('hello there');
+  //   FB.getLoginStatus(function(response) {
+  //     console.log('login state\n' + response);
+  //   });
+  // }
+  
+  render() {
+    console.log('rendered');
+    return <button onClick={this.test}>hello world</button>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,7 @@ module.exports = {
   },
   devtool: 'cheap-module-eval-source-map',
   devServer: {
+    https: true,
     contentBase: path.join(__dirname, 'public')
   }
 };
